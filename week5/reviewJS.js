4694af70067c59d4133fc853a52e88a79a214cd2
POLYDION <91535393+POLYDION@users.noreply.github.com>
2022-11-17 11:50:53 +0700

    e

---
+++ week5/reviewJS.js
@@ -0,0 +1,8 @@
+x = 10;/*ไม่มี datatype*/ 
+var y = 15;
+let myName = 'PRAEPLOY';
+const year = 2022; /*ค่าคงที่เปลี่ยนค่าของตัวแปรไม่ได้*/ 
+console.log('year เป็นชนิด',typeof(year));
+console.log('myName เป็นชนิด',typeof(myName));
+y = 'TNK';
+console.log(y,typeof(y))
---
+++ week5/reviewJS3.js
@@ -0,0 +1,7 @@
+let obj = {
+    'studentID': 64309010011,
+    'name': 'Praeploy',
+    'tel' : [080,523]
+}
+console.log(obj.name);
+console.log(obj.tel[0]);
